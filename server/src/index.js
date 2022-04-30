7059dfd4180998ef85ffc78656b639dce630c428
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2022-04-30 16:47:28 +0900

    chore: api on a multi-thread

--- server/src/index.js
+++ server/src/index.js
@@ -3,21 +3,37 @@ import http from 'http';
 import { app } from './app';
 import { insertSeeds } from './seeds';
 import { sequelize } from './sequelize';
+import cluster from 'cluster';
+import os from 'os';
+
+const numCPUs = os.cpus().length;
 
 async function main() {
-  const server = http.createServer(app);
+  // see: https://nodejs.org/dist/latest-v4.x/docs/api/cluster.html
+  if (cluster.isMaster) {
+    // データベースの初期化を
+    await sequelize.sync({
+      force: true,
+      logging: false,
+    });
+    await insertSeeds();
 
-  // データベースの初期化をします
-  await sequelize.sync({
-    force: true,
-    logging: false,
-  });
-  await insertSeeds();
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
 
-  server.listen(Number(process.env.PORT || 3000), '0.0.0.0', () => {
-    const address = server.address();
-    console.log(`Listening on ${address.address}:${address.port}`);
-  });
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`worker ${worker.process.pid} died`);
+    });
+  } else {
+    const server = http.createServer(app);
+
+    server.listen(Number(process.env.PORT || 3000), '0.0.0.0', () => {
+      const address = server.address();
+      console.log(`Listening on ${address.address}:${address.port}`);
+    });
+  }
 }
 
 main().catch(console.error);
